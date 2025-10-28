4cf7dec9aed2e8d857bf90644b6ec663372e49b1
Tonin-0 <115145716+ToniMBIX@users.noreply.github.com>
2025-10-28 23:11:12 +0100

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,20 +1,13 @@
-<?php
-
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-
-    // Deja 'allowed_origins' vacío y usa patrones:
     'allowed_origins' => [],
     'allowed_origins_patterns' => [
         '#^https://pawction-frontend\.onrender\.com$#',
         '#^http://localhost(:[0-9]+)?$#',
     ],
-
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-
-    // Aunque uses Bearer tokens, dejarlo en true no molesta
     'supports_credentials' => true,
 ];
