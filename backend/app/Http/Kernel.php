2c6e29796179e2c09a6a09bd75cdf23ef2050da0
ToniMBIX <antmarbou@alu.edu.gva.es>
2025-10-29 00:09:36 +0100

    fix

--- backend/app/Http/Kernel.php
+++ backend/app/Http/Kernel.php
@@ -3,30 +3,26 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
-use Illuminate\Http\Middleware\HandleCors; // <—
-
+use Illuminate\Http\Middleware\HandleCors;
 
 class Kernel extends HttpKernel
 {
     protected $middleware = [
-        \Fruitcake\Cors\HandleCors::class,
-        HandleCors::class, // <— CORS global
-
+        HandleCors::class,
+        \Illuminate\Foundation\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \App\Http\Middleware\TrimStrings::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
     protected $middlewareGroups = [
         'web' => [
-            \Illuminate\Cookie\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            // ...
         ],
+
         'api' => [
-            HandleCors::class, // <— CORS ANTES que todo
+            HandleCors::class, // <--- MUY IMPORTANTE: antes que nada
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Fruitcake\Cors\HandleCors::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
