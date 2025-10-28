adf2caa6a0d9a7005485e3a551046586cac46c04
Tonin-0 <115145716+ToniMBIX@users.noreply.github.com>
2025-10-28 22:26:11 +0100

    Update cors.php

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL','http://localhost:5173')],
+    'allowed_origins' => [env('https://pawction-frontend.onrender.com','http://localhost:5173')],
 
     'allowed_origins_patterns' => [],
 
