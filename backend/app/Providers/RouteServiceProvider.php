8d1b5112e1fd934e91b162294b952ec939441517
ToniMBIX <antmarbou@alu.edu.gva.es>
2025-10-29 19:26:54 +0100

    fix

--- backend/app/Providers/RouteServiceProvider.php
+++ backend/app/Providers/RouteServiceProvider.php
@@ -3,9 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -14,14 +15,19 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            // Aquí dejas el registro de rutas como lo tengas (api.php / web.php)
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
         });
     }
 
     protected function configureRateLimiting(): void
     {
         RateLimiter::for('api', function (Request $request) {
-            return [ Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip()) ];
+            return [Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip())];
         });
     }
 }
