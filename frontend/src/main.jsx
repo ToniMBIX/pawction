12572ec5e81e559cd5d2990c2f85704dd74fb8ec
Tonin-0 <antmarbou@alu.edu.gva.es>
2025-10-31 16:35:56 +0100

    fix venga ya

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './styles/index.css'
+import './index.css'
 
 import AuctionDetail from './pages/AuctionDetail.jsx'
-// importa aquí tus otras páginas/rutas
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/auctions/:id" element={<AuctionDetail />} />
-        {/* Más rutas */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
