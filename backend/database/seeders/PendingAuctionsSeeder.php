d8dc5dab95073743b0dd596642ab291beb1a8ee1
Tonin-0 <antmarbou@alu.edu.gva.es>
2025-12-06 17:46:33 +0100

    pruiebadefinitivaostia

---
+++ backend/database/seeders/PendingAuctionsSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Auction;
+
+class PendingAuctionsSeeder extends Seeder
+{
+    public function run()
+    {
+        // Usuario ganador (ID real)
+        $winnerId = 3;
+
+        // IDs de subastas que quieres marcar como ganadas
+        $auctionIds = [14, 15, 16];
+
+        foreach ($auctionIds as $id) {
+            Auction::where('id', $id)->update([
+                'winner_id' => $winnerId,
+                'is_paid'   => false,
+            ]);
+        }
+    }
+}
