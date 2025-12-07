76147a94bc6299c6e7df5ca14b151ff1700f70a5
Tonin-0 <antmarbou@alu.edu.gva.es>
2025-12-07 13:25:48 +0100

    funciona?ono?

---
+++ backend/app/Mail/PaymentCompleted.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Auction;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class PaymentCompleted extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $auction;
+
+    public function __construct(Auction $auction)
+    {
+        $this->auction = $auction;
+    }
+
+    public function build()
+    {
+        return $this->subject('Pago completado - Pawction')
+                    ->view('emails.payment_completed');
+    }
+}
