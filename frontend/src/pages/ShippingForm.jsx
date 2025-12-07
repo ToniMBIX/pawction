293e0d18dacd52478e035e92deca68a8ffb01a1c
Tonin-0 <antmarbou@alu.edu.gva.es>
2025-12-07 16:08:16 +0100

    sera?

--- frontend/src/pages/ShippingForm.jsx
+++ frontend/src/pages/ShippingForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import { ShippingAPI } from "../lib/api";
+import { ShippingAPI } from "../lib/api.js";
 
 export default function ShippingForm() {
   const { id } = useParams();
