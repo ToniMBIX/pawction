bce9e9191a5c531c1839dde378ffe852047d6a4a
Tonin-0 <antmarbou@alu.edu.gva.es>
2025-12-08 13:41:05 +0100

    arreglar pagos qr

--- backend/app/Mail/PaymentCompleted.php
+++ backend/app/Mail/PaymentCompleted.php
@@ -12,18 +12,11 @@ class PaymentCompleted extends Mailable
     use Queueable, SerializesModels;
 
     public $auction;
-    public $qr;
 
     public function __construct(Auction $auction)
     {
         $this->auction = $auction;
 
-        // QR en base64 apuntando al PDF del animal
-        $this->qr = base64_encode(
-            QrCode::format('png')->size(250)->generate(
-                url('/storage/animals/'.$auction->product->animal->pdf)
-            )
-        );
     }
 
     public function build()
