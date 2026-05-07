6a7ee40c8c7e656b41bac5776b2efc1c9aaeb26d
Tonin-0 <antmarbou@alu.edu.gva.es>
2026-05-08 00:38:39 +0200

    VAAAAAAA

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -3,12 +3,8 @@ set -e
 
 cd /app
 
-echo "APP_KEY=${APP_KEY:0:12}..."
-echo "DB_HOST=$DB_HOST"
-echo "DB_DATABASE=$DB_DATABASE"
-echo "DB_USERNAME=$DB_USERNAME"
-
 php artisan --version
 
-echo "Starting web server on :${PORT:-10000} ..."
-exec php artisan serve --host=0.0.0.0 --port=${PORT:-10000} -vvv
+echo "Starting PHP server on :${PORT:-10000} ..."
+
+exec php -S 0.0.0.0:${PORT:-10000} -t public public/index.php
